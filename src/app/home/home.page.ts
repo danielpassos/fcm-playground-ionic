1821d91f1f9cd993b03f7b0b25634db4831af0d8
Daniel Passos <daniel@passos.me>
2019-05-21 21:29:18 -0300

    Add push

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, Injectable } from '@angular/core';
+import { Push, PushObject, PushOptions } from '@ionic-native/push/ngx';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  styleUrls: ['home.page.scss']
 })
+@Injectable()
 export class HomePage {
+
+  constructor(private push: Push) {
+    const options: PushOptions = {
+      android: {},
+      ios: {
+        alert: 'true',
+        badge: true,
+        sound: 'false'
+      }
+    };
+
+    const pushObject: PushObject = this.push.init(options);
+
+    pushObject.on('notification')
+    .subscribe((notification: any) => console.log('Received a notification',
+      notification));
+
+    pushObject.on('registration')
+    .subscribe(
+      (registration: any) => console.log('Device registered', registration));
+
+    pushObject.on('error')
+    .subscribe(error => console.error('Error with Push plugin', error));
+
+  }
 
 }
