2d38f283ce4b171fd29e4bd246ec5f59c69d7eeb
Daniel Passos <daniel@passos.me>
2019-05-22 19:39:47 -0300

    Display messages on screen

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,6 +1,12 @@
 import { Component, Injectable } from '@angular/core';
 import { Push, PushObject, PushOptions } from '@ionic-native/push/ngx';
 
+export interface Message {
+  date: Date;
+  type: string;
+  message: string;
+}
+
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
@@ -8,6 +14,8 @@ import { Push, PushObject, PushOptions } from '@ionic-native/push/ngx';
 })
 @Injectable()
 export class HomePage {
+
+  messages = [] as Message[];
 
   constructor(private push: Push) {
     const options: PushOptions = {
@@ -24,18 +32,33 @@ export class HomePage {
     pushObject
     .on('registration')
     .subscribe((registration: any) => {
+      this.messages.push({
+          date: new Date(),
+          type: 'registration',
+          message: 'Device registered'
+        });
       console.log('Device registered', registration);
     });
 
     pushObject
     .on('notification')
     .subscribe((notification: any) => {
+      this.messages.push({
+        date: new Date(),
+        type: 'notification',
+        message: notification.title
+      });
       console.log('Received a notification', notification);
     });
 
     pushObject
     .on('error')
     .subscribe(error => {
+      this.messages.push({
+        date: new Date(),
+        type: 'error',
+        message: 'Error with Push plugin'
+      });
       console.error('Error with Push plugin', error);
     });
 
